b984bfde7739534f1425cf13acbceb7197cc23d1
lizagunina <lizagunina21@gmail.com>
2021-04-11 16:23:17 +0700

    add Converter draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import styles from './App.module.css';
+import Converter from './components/Converter';
 import Quote from './components/Quote';
 
 export function App() {
   return (
     <main className={styles.main}>
       {/* <Header /> */}
-      {/* <Converter/> */}
+      <Converter/>
       <Quote />
     </main>
   );
---
+++ src/components/Converter/Converter.test.js
@@ -0,0 +1,6 @@
+import { render, screen  } from "@testing-library/react";
+import { Converter } from "./Converter";
+
+describe('when rendered', () => {
+
+})
