4dfc35045bc8e4ad4de194b27e8404290755cfd3
lizagunina <lizagunina21@gmail.com>
2021-04-11 15:18:00 +0700

    add Quote's styles, render component

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,11 +1,13 @@
 import {quotes} from './quotes';
+import styles from './Quote.module.css';
 
 export const Quote = () => {
   const { text, author } = quotes[0];
+
   return (
-    <>
+    <footer className={styles.quote}>
       <blockquote>{text}</blockquote>
-      <cite>- {author}</cite>
-    </>
+      <cite className={styles.author}>— {author}</cite>
+    </footer>
   )
 }
