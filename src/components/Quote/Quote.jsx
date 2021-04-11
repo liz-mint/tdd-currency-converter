fa370efaf53b65d49baf05a48dd787e9299ebe90
lizagunina <lizagunina21@gmail.com>
2021-04-11 15:00:55 +0700

    add quote's author and test

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -1,5 +1,11 @@
+import {quotes} from './quotes';
+
 export const Quote = () => {
+  const { text, author } = quotes[0];
   return (
-  <blockquote>Test Quote</blockquote>
+    <>
+      <blockquote>{text}</blockquote>
+      <cite>- {author}</cite>
+    </>
   )
 }
--- src/components/Quote/Quote.test.js
+++ src/components/Quote/Quote.test.js
@@ -1,10 +1,18 @@
-import { render, screen  } from "@testing-library/react"
-import { Quote } from "./Quote"
+import { render, screen  } from "@testing-library/react";
+import { Quote } from "./Quote";
+import { quotes } from './quotes';
 
 describe('when rendered', () => {
+  const { text, author } = quotes[0];
   it('should contain an expedted text', () => {
     render(<Quote/>);
-    const result = screen.getByText(/Test Quote/);
+    const result = screen.getByText(new RegExp(text));
+    expect(result).toBeInTheDocument();
+  });
+
+  it('should contain an expected author', () => {
+    render(<Quote/>);
+    const result = screen.getByText(new RegExp(author));
     expect(result).toBeInTheDocument();
   })
 })
